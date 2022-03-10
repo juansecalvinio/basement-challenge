1f83e5cc6fba92b1d038a21944b2ce6e854dfaf3
juansecalvinio <j.s.calvinio@gmail.com>
2022-03-10 11:57:33 -0300

    Fix eslint build Vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,17 @@
 import * as React from "react";
-import type { GetStaticProps, NextPage } from "next";
-import { Box, Button, Flex, Grid, Stack, Text, Modal, ModalOverlay, useDisclosure, ModalContent, StackDivider } from "@chakra-ui/react";
+import type {GetStaticProps, NextPage} from "next";
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Stack,
+  Text,
+  Modal,
+  ModalOverlay,
+  useDisclosure,
+  StackDivider
+} from "@chakra-ui/react";
 import Image from "next/image";
 
 import logo from "../public/logo.svg";
@@ -8,31 +19,37 @@ import header from "../public/header.svg";
 import footer from "../public/footer.svg";
 import yourCart from "../public/your-cart.svg";
 import checkout from "../public/checkout.svg";
-import { Product } from "../product/types";
+import {Product} from "../product/types";
 
 interface Props {
   products: Product[];
 }
 
-const Home: NextPage<Props> = ({ products }) => {
+const Home: NextPage<Props> = ({products}) => {
 
-  const {isOpen: isModalOpen, onClose: closeModal, onOpen: openModal} = useDisclosure()
+  const {isOpen: isModalOpen, onClose: closeModal, onOpen: openModal} = useDisclosure();
   const [cart, setCart] = React.useState<Product[]>([]);
   const total = React.useMemo(
     () => cart.reduce((total, product) => total + product.price, 0),
-    [cart]
-  )
+    [cart],
+  );
 
   return (
     <>
       <Stack>
-        <Stack as="nav" direction="row" alignItems="center" justifyContent="space-between" padding={4}>
+        <Stack
+          as="nav"
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          padding={4}
+        >
           <Image alt="Basement" src={logo} />
-          <Button variant="outline" onClick={openModal}>Cart ({ cart.length })</Button>
+          <Button variant="outline" onClick={openModal}>Cart ({cart.length})</Button>
         </Stack>
         <Stack as="header" padding={2}>
           <Image alt="Basement" src={header} />
-          <Text 
+          <Text
             as={"marquee" as any}
             fontSize="2xl"
             fontWeight="bold"
@@ -40,12 +57,16 @@ const Home: NextPage<Props> = ({ products }) => {
             borderColor="white"
             borderTopWidth={2}
           >
-            A man can't have enough basement swag —  A man can't have enough basement swag
+            A man can&apos;t have enough basement swag —  A man can&apos;t have enough basement swag
           </Text>
         </Stack>
         <Grid gap={2} templateColumns="repeat(auto-fit, minmax(256px, 1fr))">
-          {products.map(product => (
-            <Stack key={product.id} cursor="pointer" onClick={() => setCart(cart => cart.concat(product))}>
+          {products.map((product) => (
+            <Stack
+              key={product.id}
+              cursor="pointer"
+              onClick={() => setCart(cart => cart.concat(product))}
+            >
               <Flex
                 alignItems="center"
                 bgGradient="linear(to-b, rgba(21, 21, 21, 0) 0%, #1D1D1D 100%)"
@@ -54,21 +75,21 @@ const Home: NextPage<Props> = ({ products }) => {
                 width="100%"
               >
                 <Box>
-                  <Image 
-                    src={product.image} 
-                    alt={product.name} 
-                    layout="fixed" 
-                    width={320} 
-                    height={400} 
-                    />
+                  <Image
+                    src={product.image}
+                    alt={product.name}
+                    layout="fixed"
+                    width={320}
+                    height={400}
+                  />
                 </Box>
               </Flex>
-              <Stack 
+              <Stack
                 alignItems="center"
                 borderTopColor="white"
                 borderTopWidth={2}
                 direction="row"
-                fontSize="lg" 
+                fontSize="lg"
                 justifyContent="space-between"
               >
                 <Text>{product.name}</Text>
@@ -92,13 +113,13 @@ const Home: NextPage<Props> = ({ products }) => {
           width="100%"
           zIndex={10000}
         >
-          <Button 
+          <Button
             color="white"
-            fontFamily={'Basement Grotesque'}
+            fontFamily={"Basement Grotesque"}
             fontSize="2xl"
             fontWeight="bold"
-            marginLeft="auto" 
-            textTransform="uppercase" 
+            marginLeft="auto"
+            textTransform="uppercase"
             variant="link"
             paddingX={4}
             paddingY={2}
@@ -112,7 +133,7 @@ const Home: NextPage<Props> = ({ products }) => {
                 key={index} 
                 color="white" 
                 cursor="pointer"
-                fontFamily={'Basement Grotesque'} 
+                fontFamily={"Basement Grotesque"} 
                 fontWeight="bold" 
                 direction="row" 
                 fontSize="xl" 
@@ -126,14 +147,25 @@ const Home: NextPage<Props> = ({ products }) => {
               </Stack>
             ))}
           </Box>
-          <Stack color="white" divider={<StackDivider borderColor="white" />} direction="row" padding={4} borderTopWidth={2} borderTopColor="white" justifyContent="space-between" alignItems="center">
+          <Stack
+            color="white"
+            divider={<StackDivider borderColor="white" />}
+            direction="row"
+            padding={4}
+            borderTopWidth={2}
+            borderTopColor="white"
+            justifyContent="space-between"
+            alignItems="center"
+          >
             <Text
-              fontFamily={'Basement Grotesque'}
+              fontFamily={"Basement Grotesque"}
               fontWeight="bold"
               flex={1}
               textTransform="uppercase"
               fontSize="xl"
-            >Total: $ {total}</Text>
+            >
+              Total: $ {total}
+            </Text>
             <Button variant="link">
               <Image src={checkout} width={150} />
             </Button>
@@ -145,13 +177,12 @@ const Home: NextPage<Props> = ({ products }) => {
 };
 
 export const getStaticProps: GetStaticProps<Props, any> = async () => {
-  const products: Product[] = await import('../product/mock.json').then(res => res.default);
-  
+  const products: Product[] = await import('../product/mock.json').then((res) => res.default);
   return {
     props: {
       products,
-    }
-  }
-}
+    },
+  };
+};
 
 export default Home;
